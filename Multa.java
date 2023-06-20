bf859e8dcb76af180d7b93a0792a8f99eba8c444
Bolaños Fajardo Tomás Steven <tbolanos@espol.edu.ec>
2023-06-19 22:01:21 -0500

    se agrego la clase multa

---
+++ Multa.java
@@ -0,0 +1,67 @@
+
+package com.mycompany.poo4_1p_g5_mendozaespinozabolanios;
+
+public class Multa {
+    private Cliente cliente;
+    private Vehiculo vehiculo;
+    private String infraccion;
+    private int valorMulta;
+    private String fechaInfraccion;
+    private String fechaNotificacion;
+    private int puntos;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public String getInfraccion() {
+        return infraccion;
+    }
+
+    public void setInfraccion(String infraccion) {
+        this.infraccion = infraccion;
+    }
+
+    public int getValorMulta() {
+        return valorMulta;
+    }
+
+    public void setValorMulta(int valorMulta) {
+        this.valorMulta = valorMulta;
+    }
+
+    public String getFechaInfraccion() {
+        return fechaInfraccion;
+    }
+
+    public void setFechaInfraccion(String fechaInfraccion) {
+        this.fechaInfraccion = fechaInfraccion;
+    }
+
+    public String getFechaNotificacion() {
+        return fechaNotificacion;
+    }
+
+    public void setFechaNotificacion(String fechaNotificacion) {
+        this.fechaNotificacion = fechaNotificacion;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+}
---
+++ POO4_1P_G5_MendozaEspinozaBolanios.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.poo4_1p_g5_mendozaespinozabolanios;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class POO4_1P_G5_MendozaEspinozaBolanios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        }
+}
