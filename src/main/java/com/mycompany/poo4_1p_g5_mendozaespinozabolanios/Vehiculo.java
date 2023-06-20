72586ae759791ec127ba0d84b5d455e16e492eb5
nahinespinoza <nahiespi@espol.edu.ec>
2023-06-19 20:13:28 -0500

    Clase Vehiculo

---
+++ src/main/java/com/mycompany/poo4_1p_g5_mendozaespinozabolanios/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo4_1p_g5_mendozaespinozabolanios;
+
+/**
+ *
+ * @author User
+ */
+public class Vehiculo {
+    private int  dueño;
+    private String placa;
+    private String modelo;
+    private int año;
+    private String chasis;
+    private String color;
+}
