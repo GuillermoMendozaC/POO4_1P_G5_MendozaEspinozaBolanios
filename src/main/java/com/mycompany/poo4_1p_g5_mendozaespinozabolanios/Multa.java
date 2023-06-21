7030421b77dbcba7ba62e5aa3c52259b5468a8e0
Bolaños Fajardo Tomás Steven <tbolanos@espol.edu.ec>
2023-06-21 16:46:44 -0500

    He agregado la clase multa con getters y setters

---
+++ src/main/java/com/mycompany/poo4_1p_g5_mendozaespinozabolanios/Multa.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo4_1p_g5_mendozaespinozabolanios;
+
+public class Multa {
+    private Cliente cliente;
+    private Vehiculo vehiculo;
+    private String infraccion;
+    private int valorMulta;
+    private String fechaInfraccion;
+    private String fechaNotificacion;
+    private int puntos;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public String getInfraccion() {
+        return infraccion;
+    }
+
+    public void setInfraccion(String infraccion) {
+        this.infraccion = infraccion;
+    }
+
+    public int getValorMulta() {
+        return valorMulta;
+    }
+
+    public void setValorMulta(int valorMulta) {
+        this.valorMulta = valorMulta;
+    }
+
+    public String getFechaInfraccion() {
+        return fechaInfraccion;
+    }
+
+    public void setFechaInfraccion(String fechaInfraccion) {
+        this.fechaInfraccion = fechaInfraccion;
+    }
+
+    public String getFechaNotificacion() {
+        return fechaNotificacion;
+    }
+
+    public void setFechaNotificacion(String fechaNotificacion) {
+        this.fechaNotificacion = fechaNotificacion;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+}
