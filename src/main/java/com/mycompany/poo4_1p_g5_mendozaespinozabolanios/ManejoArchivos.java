378daa91dd8e707c40be77ce95590569e3e28640
GuillermoMendozaC <guimcorn@espol.edu.ec>
2023-06-23 22:13:59 -0500

    Se carga el archivo de multas

---
+++ src/main/java/com/mycompany/poo4_1p_g5_mendozaespinozabolanios/ManejoArchivos.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo4_1p_g5_mendozaespinozabolanios;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+/**
+ *
+ * @author Lenovo
+ */
+public class ManejoArchivos {
+    public static ArrayList<String> LeeFichero(String nombrearchivo) {
+        ArrayList<String> lineas = new ArrayList<>();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File(nombrearchivo);
+            fr = new FileReader(archivo,StandardCharsets.UTF_8);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+                lineas.add(linea);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return lineas;
+
+    }
+    
+}
--- src/main/java/com/mycompany/poo4_1p_g5_mendozaespinozabolanios/PLATAFORMA.java
+++ src/main/java/com/mycompany/poo4_1p_g5_mendozaespinozabolanios/PLATAFORMA.java
@@ -3,25 +3,51 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.poo4_1p_g5_mendozaespinozabolanios;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 /**
  *
  * @author Lenovo
  */
 public class PLATAFORMA {
-    
+ static ArrayList<Multa> multas;
     public static void main(String[] args){
-        //iniciar sesion
-        //mostrar menu
-        //validar usuario y contrasenia
+        multas= new ArrayList<>();
+
     }
-   public void PLATAFORMA(){
-   
-   }
    
     public void validarUsuario(String usuario, String contrasenia){
+        
      }
     public void iniciarSesion(String usuario, String contrasenia){
         validarUsuario(usuario,contrasenia);
         }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void cargarMultas(){
+        ArrayList<String> datos=ManejoArchivos.LeeFichero("multas.txt");
+        for(String line:datos){
+            String[] elementos= line.trim().split(",");
+            Multa m = new Multa(Integer.parseInt(elementos[0]),elementos[1],elementos[2],
+                    Integer.parseInt(elementos[3]),elementos[4],elementos[5],
+                    Integer.parseInt(elementos[6]));
+            multas.add(m);
+        }
+            
+    }
 }
